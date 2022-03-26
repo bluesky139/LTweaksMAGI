0810a3af0b4f5a5047677f3cc1eec1920d6958ef
bluesky139 <bluesky139@gmail.com>
2022-03-26 19:44:15 +0800

    [GMS] No system update popup.

---
+++ app/src/main/java/li/lingfeng/magi/tweaks/google/GMSNoUpdatePopup.java
@@ -0,0 +1,30 @@
+package li.lingfeng.magi.tweaks.google;
+
+import android.app.IApplicationThread;
+import android.app.ProfilerInfo;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import li.lingfeng.lib.AppLoad;
+import li.lingfeng.magi.prefs.PackageNames;
+import li.lingfeng.magi.tweaks.base.Result;
+import li.lingfeng.magi.tweaks.base.TweakBase;
+import li.lingfeng.magi.utils.Logger;
+
+@AppLoad(packageName = PackageNames.GMS, pref = "gms_no_update_popup")
+public class GMSNoUpdatePopup extends TweakBase {
+
+    private static final String UPDATE_POPUP = "com.google.android.gms.update.phone.PopupDialog";
+
+    @Override
+    public Result startActivity(IApplicationThread caller, String callingPackage, String callingFeatureId, Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode, int flags, ProfilerInfo profilerInfo, Bundle options) throws RemoteException {
+        return new Result().before((r) -> {
+            if (intent.getComponent() != null && intent.getComponent().getClassName().equals(UPDATE_POPUP)) {
+                Logger.v("No update popup.");
+                r.setResult(0);
+            }
+        });
+    }
+}
