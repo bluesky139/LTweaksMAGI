42322f657e0a796a9e8ca7b0a038007df9221a89
bluesky139 <bluesky139@gmail.com>
2022-04-04 22:29:13 +0800

    [QQ] Darken splash from share.

--- app/src/main/java/li/lingfeng/magi/tweaks/communication/QQDarkenSplash.java
+++ app/src/main/java/li/lingfeng/magi/tweaks/communication/QQDarkenSplash.java
@@ -11,12 +11,14 @@ import androidx.annotation.Nullable;
 import li.lingfeng.lib.AppLoad;
 import li.lingfeng.magi.prefs.PackageNames;
 import li.lingfeng.magi.tweaks.base.TweakBase;
+import li.lingfeng.magi.utils.Logger;
 import li.lingfeng.magi.utils.ViewUtils;
 
 @AppLoad(packageName = PackageNames.QQ, pref = "qq_darken_splash")
 public class QQDarkenSplash extends TweakBase {
 
     private static final String SPLASH_ACTIVITY = "com.tencent.mobileqq.activity.SplashActivity";
+    private static final String JUMP_ACTIVITY = "com.tencent.mobileqq.activity.JumpActivity";
 
     @Override
     protected boolean shouldRegisterActivityLifecycle() {
@@ -25,8 +27,10 @@ public class QQDarkenSplash extends TweakBase {
 
     @Override
     public void onActivityPostCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
-        if (activity.getClass().getName().equals(SPLASH_ACTIVITY)) {
+        String name = activity.getClass().getName();
+        if (name.equals(SPLASH_ACTIVITY) || name.equals(JUMP_ACTIVITY)) {
             ViewUtils.findAllViewByType(activity, ImageView.class).forEach(imageView -> {
+                Logger.d("Set black to " + imageView + " in " + activity);
                 imageView.setImageDrawable(new ColorDrawable(Color.BLACK));
             });
         }
