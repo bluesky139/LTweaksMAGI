371a8a0a25a6a0bd6a7ee1717640c95627878a99
bluesky139 <bluesky139@gmail.com>
2022-04-09 10:30:20 +0800

    [GMS] Try fix "No update popup".

--- app/src/main/java/li/lingfeng/magi/tweaks/google/GMSNoUpdatePopup.java
+++ app/src/main/java/li/lingfeng/magi/tweaks/google/GMSNoUpdatePopup.java
@@ -13,15 +13,19 @@ import li.lingfeng.magi.tweaks.base.Result;
 import li.lingfeng.magi.tweaks.base.TweakBase;
 import li.lingfeng.magi.utils.Logger;
 
-@AppLoad(packageName = PackageNames.GMS_UI, pref = "gms_no_update_popup")
+@AppLoad(packageName = PackageNames.GMS, pref = "gms_no_update_popup")
 public class GMSNoUpdatePopup extends TweakBase {
 
     private static final String UPDATE_POPUP = "com.google.android.gms.update.phone.PopupDialog";
+    private static final String ACTION_UPDATE = "android.settings.SYSTEM_UPDATE_COMPLETE";
 
     @Override
     public Result startActivity(IApplicationThread caller, String callingPackage, String callingFeatureId, Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode, int flags, ProfilerInfo profilerInfo, Bundle options) throws RemoteException {
         return new Result().before((r) -> {
-            if (intent.getComponent() != null && intent.getComponent().getClassName().equals(UPDATE_POPUP)) {
+            Logger.d("GMS startActivity.");
+            Logger.intent(intent);
+            if (ACTION_UPDATE.equals(intent.getAction())
+                    || (intent.getComponent() != null && intent.getComponent().getClassName().equals(UPDATE_POPUP))) {
                 Logger.v("No update popup.");
                 r.setResult(0);
             }
