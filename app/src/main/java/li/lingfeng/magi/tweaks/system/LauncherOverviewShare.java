d0a93f9f93f570629429f964cd2846b8ec7a1afa
bluesky139 <bluesky139@gmail.com>
2022-11-20 20:21:33 +0800

    [Pixel Launcher] Compatible with Android 12.1.

--- app/src/main/java/li/lingfeng/magi/tweaks/system/LauncherOverviewShare.java
+++ app/src/main/java/li/lingfeng/magi/tweaks/system/LauncherOverviewShare.java
@@ -1,15 +1,21 @@
 package li.lingfeng.magi.tweaks.system;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import li.lingfeng.lib.AppLoad;
 import li.lingfeng.magi.prefs.PackageNames;
 import li.lingfeng.magi.tweaks.base.TweakBase;
+import li.lingfeng.magi.utils.ContextUtils;
 import li.lingfeng.magi.utils.Logger;
+import li.lingfeng.magi.utils.ReflectUtils;
 import li.lingfeng.magi.utils.ViewUtils;
 
 @AppLoad(packageName = PackageNames.PIXEL_LAUNCHER, pref = "launcher_overview_share")
@@ -22,14 +28,45 @@ public class LauncherOverviewShare extends TweakBase {
 
     @Override
     public void onActivityPostCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
-        View view = ViewUtils.findViewByName(activity, "action_share");
-        if (view != null) {
-            Logger.i("Set " + view + " to be visible.");
-            view.setVisibility(View.VISIBLE);
-            view = ViewUtils.findViewByName(activity, "oav_three_button_space");
+        try {
+            View view = ViewUtils.findViewByName(activity, "action_select");
             if (view != null) {
-                view.setVisibility(View.INVISIBLE);
+                Logger.i("Replace select button to share button.");;
+                Button button = (Button) view;
+                button.setText("Share");
+                Drawable icon = ContextUtils.getDrawable("ic_share");
+                button.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
+                button.setOnClickListener(v -> {
+                    Logger.d("Share button click.");
+                    try {
+                        View overviewPanel = ViewUtils.findViewByName(activity, "overview_panel");
+                        int page = (int) ReflectUtils.callMethod(overviewPanel, "getNextPage");
+                        if (page >= 0) {
+                            View taskView = (View) ReflectUtils.callMethod(overviewPanel, "getTaskViewAt", new Object[] {
+                                    page
+                            }, new Class[] {
+                                    int.class
+                            });
+                            View snapshotView = (View) ReflectUtils.callMethod(taskView, "getThumbnail");
+                            Object overlay = ReflectUtils.callMethod(snapshotView, "getTaskOverlay");
+                            Object imageApi = ReflectUtils.getObjectField(overlay, "mImageApi");
+                            Object bitmapSupplier = ReflectUtils.getObjectField(imageApi, "mBitmapSupplier");
+                            Bitmap bitmap = (Bitmap) ReflectUtils.callMethod(bitmapSupplier, "get");
+                            ReflectUtils.callMethod(imageApi, "startShareActivity", new Object[] {
+                                    new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight())
+                            }, new Class[] {
+                                    Rect.class
+                            });
+                        } else {
+                            Logger.e("page " + page);
+                        }
+                    } catch (Throwable e) {
+                        Logger.e("Share button click exception.", e);
+                    }
+                });
             }
+        } catch (Throwable e) {
+            Logger.e("LauncherOverviewShare exception.", e);
         }
     }
 }
