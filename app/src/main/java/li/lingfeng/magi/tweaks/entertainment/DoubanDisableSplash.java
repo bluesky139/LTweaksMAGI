7b9b10b57137b278c9c93f61cec26852933b1f0a
bluesky139 <bluesky139@gmail.com>
2022-11-24 18:15:15 +0800

    [Douban] Compatible with v7.39.0.

--- app/src/main/java/li/lingfeng/magi/tweaks/entertainment/DoubanDisableSplash.java
+++ app/src/main/java/li/lingfeng/magi/tweaks/entertainment/DoubanDisableSplash.java
@@ -1,35 +1,30 @@
 package li.lingfeng.magi.tweaks.entertainment;
 
-import android.app.Activity;
+import android.app.IApplicationThread;
+import android.app.ProfilerInfo;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import li.lingfeng.lib.AppLoad;
 import li.lingfeng.magi.prefs.PackageNames;
+import li.lingfeng.magi.tweaks.base.Result;
 import li.lingfeng.magi.tweaks.base.TweakBase;
 import li.lingfeng.magi.utils.Logger;
 
 @AppLoad(packageName = PackageNames.DOUBAN, pref = "douban_disable_splash")
 public class DoubanDisableSplash extends TweakBase {
 
-    private static final String SPLASH_ACTIVITY = "com.douban.frodo.activity.SplashActivity";
-    private static final String MAIN_ACTIVITY = "com.douban.frodo.MainActivity";
+    private static final String SPLASH_ACTIVITY = "com.douban.frodo.splash.SplashActivityHot";
 
     @Override
-    protected boolean shouldRegisterActivityLifecycle() {
-        return true;
-    }
-
-    @Override
-    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
-        if (activity.getClass().getName().equals(SPLASH_ACTIVITY)) {
-            Logger.v("Skip splash.");
-            Intent intent = new Intent();
-            intent.setClassName(activity, MAIN_ACTIVITY);
-            activity.startActivity(intent);
-            activity.finish();
-        }
+    public Result startActivity(IApplicationThread caller, String callingPackage, String callingFeatureId, Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode, int flags, ProfilerInfo profilerInfo, Bundle options) throws RemoteException {
+        return new Result().before((r) -> {
+            if (intent.getComponent() != null && intent.getComponent().getClassName().equals(SPLASH_ACTIVITY)) {
+                Logger.v("Disable " + SPLASH_ACTIVITY);
+                r.setResult(0);
+            }
+        });
     }
 }
